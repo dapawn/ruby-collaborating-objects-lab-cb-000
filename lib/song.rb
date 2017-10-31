c9b1f91f65d7b0f37b4941c81ab92e12a8faa267
David Kuruvilla <dapawn@gmail.com>
2017-10-31 14:19:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,14 +36,12 @@ class Song
   end
 
   def self.new_by_name(name)
-      song =  self.new
-      song.name = name
+      song =  self.new(name)
       song
   end
 
   def self.create_by_name(name)
-      song = self.create
-      song.name = name
+      song = self.create(name)
       song
   end
 
